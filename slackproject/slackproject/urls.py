bd1d1d2666b9e8bc5767e04451eedaeec996fb86
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2024-09-05 11:28:50 +0900

    環境構築

--- slackproject/slackproject/urls.py
+++ slackproject/slackproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for slack project.
+URL configuration for slackproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
